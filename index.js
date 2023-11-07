0554d47c1026afb93763a655a412bab0b2075ee0
Emilija <145319859+Emilijadru@users.noreply.github.com>
2023-11-07 12:48:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const span = document.querySelector('span')
+const btn = document.querySelector('.btn')
+const body = document.querySelector('body')
+
+const generateHexColor = ()=> {
+    const randomColor = Math.random().toString(16).slice(2,8)
+
+    span.innerHTML = '#' + randomColor
+
+    body.style.backgroundColor = '#' + randomColor
+    console.log(randomColor);
+}
+
+btn.addEventListener('click', generateHexColor)
+
+body.addEventListener('mousewheel', generateHexColor)
